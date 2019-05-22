64279262cd53011e54378fd05a980a052f2686b8
Eldar Babaev <eldar.babaev@softomate.com>
2019-05-22 07:42:33 +0700

    Changed isPrime function

--- src/games/prime-data.js
+++ src/games/prime-data.js
@@ -4,12 +4,13 @@ import { randomInt } from '../utils';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  for (let i = 2; i <= num; i += 1) {
-    if (num % i === 0 && i !== num) {
-      return true;
+  if (num < 2) return false;
+  for (let i = 2; i * i < num; i += 1) {
+    if (num % i === 0) {
+      return false;
     }
   }
-  return false;
+  return true;
 };
 
 const makeData = () => {
